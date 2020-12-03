4dc4c425232ef111e0c26b35b04738fbe201655b
wasiq-17 <s.wazi044@gmail.com>
2020-12-03 01:34:27 -0800

    github profile search

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import GithubSearch from './component/GithubSearch'
+function App() {
+  return (
+    <div >
+    <GithubSearch/>
+    </div>
+  );
+}
+
+export default App;
